3b7402bbe6f38182ce2c2c7ab2e62c84460bff9e
Tanvi Dedhia <142569541+thetanvi18@users.noreply.github.com>
2023-08-30 19:08:09 +0530

    Update task 1.3.py

--- task 1.3.py
+++ task 1.3.py
@@ -1,10 +1,12 @@
 #Atomic Codebreaker
 encoded_lists = [[1,2,3,4,6],
                  [5,7,8,9,15],
-                 [13,14,16,18,19,20],
-                 [10,11,12,17,21,22,23]
+                 [12,14,16,18],
+                 [10,11,12,13,16,17,18,20]
                  ]
 #code for explode_chains
-
+for encoded_list in encoded_lists:
+    exploded_list = []
+    i = 0
 result = explode_chains(encoded_lists)
 print(result)
